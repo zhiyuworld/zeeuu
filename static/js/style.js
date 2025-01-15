d746ddfefacb9ad60c734869100f9ee3582694d6
zhiyuworld <zhiyuworld@qq.com>
2025-01-15 10:20:14 +0000

    website

---
+++ static/js/style.js
@@ -0,0 +1,11 @@
+/*网站制作QQ43171398*/
+yen_menu('.yen-menu');
+function yen_menu(ulclass){
+	$(document).ready(function(){
+		$(ulclass+' li').hover(function(){
+			$(this).children("ul").show();
+		},function(){
+			$(this).children("ul").hide();
+		});
+	});
+}
